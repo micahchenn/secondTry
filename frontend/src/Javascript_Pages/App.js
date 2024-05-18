a860207cc06bd73f4e1b0b7a8253a03373beb8ea
Micah Chen <micahchen@utexas.edu>
2024-05-18 15:07:44 -0500

    Setup

---
+++ frontend/src/Javascript_Pages/App.js
@@ -0,0 +1,14 @@
+import '../Styling_Pages/App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to the Front Page</h1>
+    </div>
+  );
+}
+
+
+
+export default App;
