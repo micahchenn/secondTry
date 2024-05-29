9b9028a0a018254a9128d52e9d863ebef83da646
Micah Chen <micahchen@utexas.edu>
2024-05-29 10:46:19 -0500

    misc

--- frontend/src/Javascript_Pages/Private_Pages/Stock_Watchlist.jsx
+++ frontend/src/Javascript_Pages/Private_Pages/Stock_Watchlist.jsx
@@ -1,16 +1,13 @@
 import React, { useState, useEffect } from 'react';
-import { Line } from 'react-chartjs-2';
-import { Chart as ChartJS, TimeScale, LinearScale, LineElement, PointElement, Tooltip, Legend } from 'chart.js';
 import 'chartjs-adapter-date-fns';
-import zoomPlugin from 'chartjs-plugin-zoom';
 import api from '../../api';
 import '../../Styling_Pages/Private_Pages/Stock_Watchlist.css'; // Import the CSS file
 import 'chartjs-chart-financial';
-import crosshairPlugin from 'chartjs-plugin-crosshair';
 import greenBlur from '../../Styling_Pages/Public_Pictures/Green_Blur.png';
 import redBlur from '../../Styling_Pages/Public_Pictures/Red_Blur.png';
+import Stock_Line_Graph from '../Static_Elements/Stock_Line_Graph';
+import News from '../Static_Elements/News';
 
-ChartJS.register(TimeScale, LinearScale, LineElement, PointElement, Tooltip, Legend, zoomPlugin, crosshairPlugin);
 
 function Stock_Watchlist() {
     const [data, setData] = useState(null);
@@ -20,6 +17,10 @@ function Stock_Watchlist() {
     const [searchResults, setSearchResults] = useState([]);
     const [searchTimeout, setSearchTimeout] = useState(null);
     const [selectedStock, setSelectedStock] = useState(null);
+    const [newsSentiment, setNewsSentiment] = useState(null); // State for storing the news sentiment
+
+
+
 
     let mostRecentPrice;
     if (data) {
@@ -78,152 +79,52 @@ function Stock_Watchlist() {
             setSelectedStock(selected);
             setJsonResponse(JSON.stringify(response.data, null, 2));
             setError(null);
+            const newsSentimentResponse = await api.get(`/stocks/news-sentiment?tickers=${symbol}`);
+            setNewsSentiment(newsSentimentResponse.data);
         } catch (error) {
             setError(error.message);
             setData(null);
         }
     };
 
-    const chartData = {
-        labels: data ? Object.keys(data).reverse() : [],
-        datasets: [
-            {
-                label: 'Stock Price',
-                data: data ? Object.values(data).map(item => item['4. close']).reverse() : [],
-                fill: false,
-                backgroundColor: color, // Change the color here
-                borderColor: color,
-                pointRadius: 0,
-            },
-        ],
-    };
-
-    const options = {
-        scales: {
-            x: {
-                type: 'time',
-                time: {
-                    unit: 'day',
-                    tooltipFormat: 'MM/dd/yyyy',
-                    max: data ? Object.keys(data)[0] : null,
-                },
-                grid: {
-                    display: false,
-                },
-                ticks: {
-                    maxRotation: 0,
-                    autoSkip: true,
-                    maxTicksLimit: 5 // Adjust this number to the maximum number of ticks you want to display
-                },
-            },
-            y: {
-                type: 'linear',
-                beginAtZero: false,
-                grid: {
-                    display: false,
-                },
-                ticks: {
-                    callback: function (value) {
-                        return value.toFixed(2); // Format the y-axis labels to have two decimal points
-                    }
-                },
-            },
-        },
-        plugins: {
-            crosshair: {
-                line: {
-                    color: color,
-                    width: 1
-                },
-                sync: {
-                    enabled: false,
-                },
-                zoom: {
-                    enabled: false,
-                }
-            },
-            tooltip: {
-                enabled: true,
-                mode: 'index',
-                intersect: false,
-                callbacks: {
-                    label: function (context) {
-                        var label = context.dataset.label || '';
-
-                        if (label) {
-                            label += ': ';
-                        }
-                        if (context.parsed.y !== null) {
-                            label += new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(context.parsed.y);
-                        }
-                        return label;
-                    }
-                }
-            },
-            zoom: {
-                limits: {
-                    x: { min: 'original', max: 'original' },
-                    y: { min: 'original', max: 'original' },
-                },
-                pan: {
-                    enabled: true,
-                    mode: 'xy',
-                    overScaleMode: 'none',
-                },
-                zoom: {
-                    wheel: {
-                        enabled: true,
-                    },
-                    pinch: {
-                        enabled: true,
-                    },
-                    mode: 'xy',
-                    overScaleMode: 'none',
-                },
-            },
-        },
-        responsive: true,
-        maintainAspectRatio: false,
-    };
-
     return (
         <div className="stock-watchlist">
-            <div 
-                className="blur-effect" 
-                style={{ backgroundImage: `url(${blurImage})` }} // Set the background image here 
-            ></div> {/* This will be the blurred background */}
-            <div className="left-aligned-content">
-                {selectedStock && <h2 className="stock-name-header">{selectedStock['2. name']}</h2>}
-                {mostRecentPrice && <p className="price-header">Most recent price: ${mostRecentPrice}</p>}
-                <div className="chart-container">
-                    {data && <Line data={chartData} options={options} />}
+            <div className="blur-effect"></div>
+            <div className="grid-item">
+                <div className="left-aligned-content">
+                    {selectedStock && <h2 className="stock-name-header">{selectedStock['2. name']}</h2>}
+                    {mostRecentPrice && <p className="price-header">Most recent price: ${mostRecentPrice}</p>}
+                    <Stock_Line_Graph data={data} color={color} />
+                    <div className="search-container">
+                        <form onSubmit={(e) => { e.preventDefault(); fetchData(searchTerm); }}>
+                            <input 
+                                type="text" 
+                                value={searchTerm} 
+                                onChange={handleSearchChange} 
+                                placeholder="Search for a stock..." 
+                            />
+                            <button type="submit">Submit</button>
+                        </form>
+                        {searchResults.length > 0 && (
+                            <select onChange={(e) => fetchData(e.target.value)} className="dropdown-menu">
+                                <option>Select a stock...</option>
+                                {searchResults.map(result => (
+                                    <option key={result['1. symbol']} value={result['1. symbol']}>
+                                        {result['1. symbol']} - {result['2. name']}
+                                    </option>
+                                ))}
+                            </select>
+                        )}
+                    </div>
+                    {error && <p>Error: {error}</p>}
+                    <pre>{jsonResponse}</pre>
                 </div>
-                <div className="search-container">
-                    <form onSubmit={(e) => { e.preventDefault(); fetchData(searchTerm); }}>
-                        <input 
-                            type="text" 
-                            value={searchTerm} 
-                            onChange={handleSearchChange} 
-                            placeholder="Search for a stock..." 
-                        />
-                        <button type="submit">Submit</button>
-                    </form>
-                    {searchResults.length > 0 && (
-                        <select onChange={(e) => fetchData(e.target.value)} className="dropdown-menu">
-                            <option>Select a stock...</option>
-                            {searchResults.map(result => (
-                                <option key={result['1. symbol']} value={result['1. symbol']}>
-                                    {result['1. symbol']} - {result['2. name']}
-                                </option>
-                            ))}
-                        </select>
-                    )}
-                </div>
-                {error && <p>Error: {error}</p>}
-                <pre>{jsonResponse}</pre>
+            </div>
+            <div class="grid-item second-column-first-item">
+                <News newsSentiment={newsSentiment} />
             </div>
         </div>
     );
-}
+};
 
 export default Stock_Watchlist;
